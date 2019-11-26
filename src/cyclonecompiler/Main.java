2f4fe75c7125aa75e2fe08d3aa1fa4ad514e044c
sergi <askj545@gmail.com>
2019-11-26 16:43:07 +0100

    Prueba jflex

--- src/cyclonecompiler/Main.java
+++ src/cyclonecompiler/Main.java
@@ -2,11 +2,29 @@ package cyclonecompiler;
 
 import Parser.*;
 import Scanner.*;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Main {
     
+    @SuppressWarnings("empty-statement")
     public static void main (String [] args){
-        System.out.println("hola");
+        final String FILE_NAME = "C:\\Users\\Sergi\\Desktop\\PracticaCompiladores\\CycloneCompiler\\cyclone_src\\program.cc";
+        try {
+            
+            FileReader in = new FileReader(FILE_NAME);
+            Scanner s = new Scanner(in);
+            while(s.next_token().sym != ParserSym.EOF);
+            
+        } catch (FileNotFoundException e) {
+                System.err.println("Could not open '"+FILE_NAME+"'");
+        } catch (IOException ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        } 
+
     }
     
 }
