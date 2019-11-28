b00dc860330675fb42a59aba3b3885ae3eabead7
sergi <askj545@gmail.com>
2019-11-28 11:30:57 +0100

    Refined CUP grammar

---
+++ cyclone_src/program.cc
@@ -0,0 +1,42 @@
+/* AddTwo: Suma dos numeros
+    @param int a primer operando
+    @param int b segundo operando
+    @returns int resultado de a + b
+*/
+func int AddTwo(int a, int b)
+{
+    return a+b;
+}
+
+
+/* CountTo: Cuenta hasta un cierto objetivo e imprime la cuenta
+    @param int goal Objetivo hasta el que contar
+    @example
+        goal = 3
+        out: 1
+             2
+             3
+*/
+func void CountTo(int goal)
+{
+    int i = 1;
+    while (i <= goal)
+    {
+        i = i + 1;
+        output(i);
+    }
+}
+
+/* Función principal */
+func int main()
+{
+    int x = 2;
+    int y = 3;
+    int result = AddTwo(x , y);
+    output("Se ha contado hasta :");
+    output(result);
+    countTo(result);
+}
+
+
+
